a3d948d67b8bf9dede4bad988ac639ca514ecb07
tbagz104 <akshayhari04@gmail.com>
2020-12-06 21:48:15 +0530

    Big-Changes

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -16,7 +16,7 @@ public class Driver {
 		try {
 			
 			Class.forName("org.postgresql.Driver");
-			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/MusicCompany", "postgres", "dognose1@2");
+			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/MusicCompany", "postgres", "pass");
 			//st = connect.createStatement();
 			
 		}
